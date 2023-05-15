c96eda8301f23ef5e52732e6d23ca96639b770ab
Jason Osman <jason.osman@supersport.com>
2023-05-15 20:02:27 +0200

    creating bookmarks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,29 +1,29 @@
 class ListsController < ApplicationController
-    def index
-      @lists = List.all
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(lists_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
-      @list = List.find(params[:id])
-    end
+  private
 
-    def new
-      @list = List.new
-    end
-
-    def create
-      @list = List.new(lists_params)
-
-      if @list.save
-        redirect_to list_path(@list)
-      else
-        render :new, status: :unprocessable_entity
-     end
-    end
-
-    private
-
-    def lists_params
-      params.require(:list).permit(:name)
-    end
+  def lists_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create]
+  root 'list#index'
+  resources :lists, only: %i[:index, :show, :new, :create] do
+  end
+  resources :bookmarks, only: [:new, :create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  end
